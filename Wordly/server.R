3adb377ed7090b7548be42ba8a842d2c5c97ea0b
Jappio9000 <jrbalvers@gmail.com>
2017-06-12 21:15:15 +0200

    First (simple) working version of the Shiny app
    Not yet tested on shinyapps.io !

--- Wordly/server.R
+++ Wordly/server.R
@@ -1,37 +1,43 @@
 library(shiny)
 
+source("wordly_script.R") # the prediction script loads the 'wordly()' function
+#load("data/wordly_data.RData") # the prediction data needed in the script
+
+#try with explicit object loading
+bigram_split <- load("data/bigram.RData")
+trigram_split <- load("data/trigram.RData")
+fourgram_split <- load("data/fourgram.RData")
+
+#source("wordly.Rmd") # documentation for the app (possibly on another tab)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+
+  # this links to the 'update' button of the app
+  prediction <- eventReactive(input$update, {
+    wordly(input=input$words)[1,2]
+    #input$words
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = 10 + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = 10, col = 'darkgray', border = 'white')
+    #strsplit(tolower(input$words), " ")[[1]]
     
   })
   
-  ## here's some setup for the model
-  
-  # get the words
-  input$words 
-  
-  # if there is more than 1 word, get the last 2, 3 or 4 if available.
-  # and split so that it can be used in the model
-  
-  # make a prediction using 4-gram
-  
-  # test if prediction is strong enough 
-  
-  # otherwise go to 3-gram and test again
-  
-  # otherwise go to 2-gram and test again
-  
-  # and lastly use a placeholder word if no suitable predictions are found
-  pred = "OuterWordly!"
+  ####################################
+  #### HIER GEBLEVEN MET HET BOUWEN - HIJ LOOPT VAST OP DE OUTPUT
+  #### WANT NU MET [1,2] PAKT HIJ WEL DE EERSTE VOORSPELLING
+  #### MAAR ALLEEN ALS ER EEN VOORSPELLING IS.... ANDERS IS HET GEEN TABEL MAAR EEN STRING (ZIE SCRIPT)
+  ####################################
   
   
+  # in the final app, use renderText to only output the most likely word
+  output$prediction <- renderText ({
+  #output$prediction <- tableOutput({
+    # get the words
+    prediction()
+  })
+  
+
 })
+
+
+## demo of actionbutton:https://shiny.rstudio.com/gallery/actionbutton-demo.html
--- Wordly/ui.R
+++ Wordly/ui.R
@@ -1,37 +1,33 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Word prediction using R"),
+  titlePanel("Worldy - an next-word prediction app using R"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      selectInput("selection", "Choose a model:",
-                  choices = models),
-      textInput("words", "Words", "Thank you for", placeholder = "Type some text"),
-      actionButton("update", "Predict!")
+      #selectInput("selection", "Choose a model:",
+      #            choices = models),
+      textInput("words", "Words", "Enter your text", placeholder = "Type some text"),
+      
+      br(),
+      
+      # a button for the user to push - this updates the prediction
+      actionButton("update", "Predict!"),
+      p("Click the button to predict your next word")
     ),
     
     # Show the predicted word
     ## optional: add a wordcloud of the x most likely words
     ## optional: add a likelihood ratio (P-value or whatever we can calculate)
     mainPanel(
-       plotOutput("distPlot")
-    )
+      verbatimTextOutput("prediction")
+    )#,
     
     ## include somewhere (on another tab?) general info on the model and myself
-    includeMarkdown("wordly.Rmd")
+    #includeMarkdown("wordly.Rmd")
   )
 ))
